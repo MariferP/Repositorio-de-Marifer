2e6b6d30041b0f05dcd88b57f29add1fcb055693
MariferP <79059059+MariferP@users.noreply.github.com>
2023-08-28 21:51:52 +0000

    hice algo :D

---
+++ si.py
@@ -0,0 +1,4 @@
+print(“Esta es una prueba de programación dentro de git”)
+print(“Esta es una prueba de programación dentro de git”)
+print(quesadillas)
+print(“Esta es una prueba de programación dentro de git”)
